c04a6e22252e71c0909d22807f0458fbbc096377
pseudo_control <zgentry@pm.me>
2026-03-24 17:45:37 -0500

    Update to v0.3.4, migrate to gentry-lab org

--- Casks/ember-md.rb
+++ Casks/ember-md.rb
@@ -1,11 +1,11 @@
 cask "ember-md" do
-  version "0.3.3"
-  sha256 "7db34a5bb0305fe232caeeb0960dd28f96a029909546faefdb765f563261ffdb"
+  version "0.3.4"
+  sha256 "412f89ab0b9686a125041372be71c4b11d8f7b48da113f9da1f0de79f06dd02a"
 
-  url "https://github.com/pseudo-control/Ember-MD/releases/download/v#{version}/Ember-#{version}-mac-arm64.dmg"
+  url "https://github.com/gentry-lab/Ember-MD/releases/download/v#{version}/Ember-#{version}-mac-arm64.dmg"
   name "Ember MD"
   desc "GPU-accelerated molecular dynamics on Apple Silicon"
-  homepage "https://github.com/pseudo-control/Ember-MD"
+  homepage "https://github.com/gentry-lab/Ember-MD"
 
   depends_on arch: :arm64
 
