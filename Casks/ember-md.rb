c4e80636f18deb76eec9385fad53290ca9ea04fc
pseudo_control <zgentry@pm.me>
2026-03-25 11:15:49 -0500

    ember-md 0.3.11

--- Casks/ember-md.rb
+++ Casks/ember-md.rb
@@ -1,6 +1,6 @@
 cask "ember-md" do
-  version "0.3.10"
-  sha256 "d3c2c33dc305d6416dd43cc69dcea6406a03ee8c2b4d4c592eaca851462a82b5"
+  version "0.3.11"
+  sha256 "6566733237dd3fc6aa0e02fb59bdbff8d388ee201ad4ed2d32dd304a3c804d04"
 
   url "https://github.com/gentry-lab/Ember-MD/releases/download/v#{version}/Ember-#{version}-mac-arm64.dmg"
   name "Ember MD"
